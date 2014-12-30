c57d4a56998366677453f913f5220cb5bbf4ae34
eliorb <eliorb@codeoasis.com>
2014-12-30 11:15:15 +0200

    feat(coffee): adding coffeescript

---
+++ gulp/changelog.js
@@ -0,0 +1,23 @@
+var changelog = require('conventional-changelog'),
+    fs        = require('fs');
+
+module.exports = function (gulp, $, gutil, helpers, src, pkg) {
+  'use strict';
+  gulp.task('changelog', 'Updates changelog', function (done) {
+    function changeParsed (err, log) {
+      if (err) {
+        return done(err);
+      }
+      fs.writeFile('CHANGELOG.md', log, done);
+    }
+
+    changelog({
+      repository: pkg.repository.url,
+      version: gutil.env.to || pkg.version,
+      subtitle: pkg.subtitle,
+      from: gutil.env.version,
+      to:      gutil.env.to || 'HEAD'
+    }, changeParsed);
+
+  });
+};
